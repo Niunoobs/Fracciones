f9e5b9439205b88b2f517b2fd09978da7f63ca94
Luis Ángel Rodríguez Lugo <100735246+Niunoobs@users.noreply.github.com>
2022-03-14 09:16:11 -0500

    Add files via upload

---
+++ TAD-Interface/interfaceFraccion.java
@@ -0,0 +1,18 @@
+public interface interfaceFraccion
+{
+        public abstract void dividir(int num, int denom);
+        
+        public abstract void Multiplicar(int num, int denom);
+        
+        public abstract void Suma(int num, int denom);
+        
+        public abstract void Resta(int num, int denom);
+        
+        public abstract void raizCuadrada();
+        
+        public abstract void potencia(int num);
+        
+        public abstract double Porcentaje();
+        
+        public abstract double Decimal();
+}
